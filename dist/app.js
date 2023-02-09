a34bb38cb0f458ea7b2fea32880219d6e502df2d
laiamanda <40805270+laiamanda@users.noreply.github.com>
2023-02-08 21:05:05 -0500

    Add /dist

---
+++ dist/app.js
@@ -0,0 +1,8 @@
+"use strict";
+const form = document.querySelector('form');
+const addressInput = document.getElementById('address');
+function searchAddressHandler(event) {
+    event.preventDefault();
+}
+form.addEventListener('submit', searchAddressHandler);
+//# sourceMappingURL=app.js.map
